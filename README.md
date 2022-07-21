213bb834a32623cbd940577d052c88695b088b2c
Vinicius Dias <carlosv775@gmail.com>
2022-07-20 21:40:19 -0300

    Gerando imagem com texto a partir do poster do filme

--- README.md
+++ README.md
@@ -2,11 +2,29 @@
 
 Desafios da imersão Java da Alura implementados em PHP.
 
-## Aula 1
+## Ambiente
+
+Para executar o projeto, há um _Dockerfile_. Sendo assim, com Docker instalado, basta executar:
+
+```shell
+docker build -t php-docker .
+```
+
+E para executar os exemplos:
+```shell
+docker run --rm -itv $(pwd):/app -w /app php-docker php busca-filmes.php
+```
+
+## API Endpoint
 
 Na aula 1 foi implementada uma busca pelos principais filmes na API do IMDB.
 Para usar um endpoint diferente do padrão ("https://alura-filmes.herokuapp.com/conteudos"), basta definir a variável de ambiente `API_ENDPOINT`. Exemplo em ambientes Unix:
 
 ```shell
-API_ENDPOINT="https://api.mocki.io/v2/549a5d8b" php busca-filmes.php
+API_ENDPOINT="https://api.mocki.io/v2/549a5d8b" php aula-1.php
+```
+
+Ou usando Docker:
+```shell
+docker run --rm -itv $(pwd):/app -w /app -e API_ENDPOINT="https://api.mocki.io/v2/549a5d8b" php-docker php aula-1.php
 ```
--- busca-filmes.php
+++ busca-filmes.php
@@ -12,17 +12,19 @@ $negrito = "\u{001b}[1m";
 $corClassificacao = "\u{001b}[37m\u{001b}[45m";
 
 // Exibição dos dados recuperados
-foreach ($respostaParseada->items as $filme) {
+foreach ([$respostaParseada->items[0]] as $filme) {
     $estrelas = is_numeric($filme->imDbRating) ? notaParaEstrelas($filme->imDbRating) : '';
 
+    $imagem = imagemSemTamanho($filme->image);
     echo <<<FIM
     {$negrito}Título:{$semEstilo} $filme->title
-    {$negrito}Poster:{$semEstilo} $filme->image
+    {$negrito}Poster:{$semEstilo} $imagem
     {$negrito}{$corClassificacao}Classificação: {$filme->imDbRating}{$semEstilo}
     $estrelas
     
     
     FIM;
+    criaFigurinha($imagem);
 }
 
 function notaParaEstrelas(float $nota): string
@@ -31,4 +33,46 @@ function notaParaEstrelas(float $nota): string
     $arrayEstrelas = array_fill(0, round($nota), $emojiEstrela);
 
     return implode('', $arrayEstrelas);
+}
+
+function criaFigurinha(string $caminhoImagem): string
+{
+    // Cria a pasta saída se ainda não existir
+    $caminhoSaida = __DIR__ . '/saida';
+    if (!is_dir($caminhoSaida)) {
+        mkdir($caminhoSaida);
+    }
+
+    // Lê a imagem original (seja de arquivo ou URL)
+    $imagemOriginal = new Imagick($caminhoImagem);
+    $larguraOriginal = $imagemOriginal->getImageWidth();
+    $alturaOriginal = $imagemOriginal->getImageHeight();
+
+    // Cria a nova imagem com fundo transparente e 100px a mais de altura
+    $novaImagem = new Imagick();
+    $novaImagem->newImage(
+        $larguraOriginal,
+        $alturaOriginal + 100,
+        new ImagickPixel('transparent')
+    );
+    // Adiciona a imagem original a essa nova imagem
+    $novaImagem->compositeImage($imagemOriginal, Imagick::COMPOSITE_ADD, 0, 0);
+
+    // Escreve na nova imagem
+    $definicoesDaFonte = new ImagickDraw();
+    $definicoesDaFonte->setFont('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf');
+    $definicoesDaFonte->setFillColor(new ImagickPixel('#9b870c'));
+    $definicoesDaFonte->setFontSize(64);
+    $novaImagem->annotateImage($definicoesDaFonte, 0, $alturaOriginal + 75, 0, 'Texto de teste');
+
+    // Salva a nova imagem
+    $imagemSaida = $caminhoSaida . '/' . basename($caminhoImagem, '.jpg') . '.png';
+    $novaImagem->writeImage($imagemSaida);
+
+    return $imagemSaida;
+}
+
+function imagemSemTamanho(string $caminhoCompletoImagem): string
+{
+    return preg_replace('/\@(.*)\.jpg/', '@.jpg', $caminhoCompletoImagem);
 }
